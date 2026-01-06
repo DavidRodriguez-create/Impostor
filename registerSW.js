37ee0a3afb3eb79af31ba602fae9cedaa62604fe
DavidRodriguez-create <DavidRodriguez-create@users.noreply.github.com>
2026-01-06 02:42:22 +0000

    deploy: 3a55e5bd88b2c8114d588c912654af9a289178bb

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/impostor/sw.js', { scope: '/impostor/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
